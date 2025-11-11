1bc71dc17a3c4272d858528594ab1b2ede70c2cb
Dojbat <wavethanakit@gmail.com>
2025-11-12 00:13:04 +0700

    change the logging output to JSon format

--- social_media/logging_conf.py
+++ social_media/logging_conf.py
@@ -2,8 +2,9 @@ from logging.config import dictConfig
 
 from social_media.config import DevConfig, config
 
+
 def configure_logging() -> None:
-    dictConfig (
+    dictConfig(
         {
             "version": 1,
             "disable_existing_loggers": False,
@@ -11,57 +12,48 @@ def configure_logging() -> None:
                 "correlation_id": {
                     "()": "asgi_correlation_id.CorrelationIdFilter",
                     "uuid_length": 8 if isinstance(config, DevConfig) else 32,
-                    "default_value": "-"
+                    "default_value": "-",
                 }
             },
             "formatters": {
                 "console": {
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
-                    "format": "(%(correlation_id)s)%(name)s:%(lineno)d - %(message)s"
+                    "format": "(%(correlation_id)s)%(name)s:%(lineno)d - %(message)s",
                 },
                 "file": {
-                    "class": "logging.Formatter",
+                    "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
-                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s |  [%(correlation_id)s]%(name)s:%(lineno)d - %(message)s"
-                }
+                    "format": "%(asctime)s %(msecs)03d %(levelname)-8s %(correlation_id)s %(name)s %(lineno)d %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                    "filters": ["correlation_id"]
+                    "filters": ["correlation_id"],
                 },
                 "rotating_file": {
                     "class": "logging.handlers.RotatingFileHandler",
                     "level": "DEBUG",
                     "formatter": "file",
                     "filename": "social_media.log",
-                    "maxBytes": 1024 * 1024 , # 1MB
+                    "maxBytes": 1024 * 1024,  # 1MB
                     "backupCount": 5,
                     "encoding": "utf8",
-                    "filters": ["correlation_id"]
-                }
+                    "filters": ["correlation_id"],
+                },
             },
             "loggers": {
-                "uvicorn": {
-                    "handlers": ["default", "rotating_file"],
-                    "level": "INFO"
-                },
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
                 "social_media": {
                     "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
-                    "propagate": False
+                    "propagate": False,
                 },
-                "databases": {
-                    "handlers": ["default"],
-                    "level": "WARNING"
-                },
-                "aiosqlite": {
-                    "handlers": ["default"],
-                    "level": "WARNING"
-                }
-            }
+                "databases": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
+            },
         }
     )
